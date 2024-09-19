c59087e6fb3e0bfa42f53cc8a184d61dc1f13cec
siegfriedbz <siegfried.bozza@yahoo.com>
2024-09-19 20:05:35 +0200

    add footer & header

---
+++ app/_components/Footer.tsx
@@ -0,0 +1,36 @@
+const TELEGRAM_URL = process.env.NEXT_PUBLIC_TELEGRAM_URL;
+const X_URL = process.env.NEXT_PUBLIC_X_URL;
+const GITHUB_URL = process.env.NEXT_PUBLIC_GITHUB_URL;
+
+const Footer = () => {
+  return (
+    <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      <a
+        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+        href={TELEGRAM_URL}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Telegram
+      </a>
+      <a
+        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+        href={X_URL}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        X
+      </a>
+      <a
+        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+        href={GITHUB_URL}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Github
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
