dc7450363ee8bc11477886fe2359babab7687178
siegfriedbz <siegfried.bozza@yahoo.com>
2024-09-21 11:35:58 +0200

    move  store init logic to StoreProvider to keep rootLayout server-componentr

--- app/_store/StoreProvider.tsx
+++ app/_store/StoreProvider.tsx
@@ -1,13 +1,23 @@
 "use client";
 
+import { useEffect } from "react";
 import { Provider } from "react-redux";
-import store from "./index";
+import { initializeSubscriptions, unsubscribeAll } from "./subscriptions";
+import { store } from "./store";
 
 type TProps = {
   children: React.ReactNode;
 };
 
 const StoreProvider = ({ children }: TProps) => {
+  // Initialize store
+  useEffect(() => {
+    initializeSubscriptions(store);
+    return () => {
+      unsubscribeAll();
+    };
+  }, []);
+
   return <Provider store={store}>{children}</Provider>;
 };
 
