ba8337b6b09056b1d1eaa18a108f8c49233ec110
Thomas <thomas@dcts.dev>
2024-09-21 03:25:26 +0200

    rename Header to Navbar

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Header = () => {
+const Navbar = () => {
   return <div></div>;
 };
 
-export default Header;
+export default Navbar;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Header from "./_components/Header";
+import Navbar from "./_components/Navbar";
 import Footer from "./_components/Footer";
 import StoreProvider from "./_store/StoreProvider";
 
@@ -33,7 +33,7 @@ export default function RootLayout({
       >
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
           <StoreProvider>
-            <Header />
+            <Navbar />
             {children}
             <Footer />
           </StoreProvider>
