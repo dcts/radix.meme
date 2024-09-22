71ba125de65651b1615ffbd5d2db5057789dc692
saidam90 <saida.murtazaliyeva@yahoo.com>
2024-09-23 01:51:12 +0200

    Add icons in the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,34 +1,30 @@
-const TELEGRAM_URL = process.env.NEXT_PUBLIC_TELEGRAM_URL || "";
-const X_URL = process.env.NEXT_PUBLIC_X_URL || "";
-const GITHUB_URL = process.env.NEXT_PUBLIC_GITHUB_URL || "";
+import Image from "next/image";
+
+//TODO: add social media links
 
 const Footer = () => {
+  const socials = [
+    {
+      id: "telegram",
+      url: process.env.NEXT_PUBLIC_TELEGRAM_URL || "",
+    },
+    {
+      id: "github",
+      url: process.env.NEXT_PUBLIC_GITHUB_URL || "",
+    },
+    {
+      id: "x",
+      url: process.env.NEXT_PUBLIC_X_URL || "",
+    },
+  ];
+
   return (
     <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-      <a
-        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-        href={TELEGRAM_URL}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Telegram
-      </a>
-      <a
-        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-        href={X_URL}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        X
-      </a>
-      <a
-        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-        href={GITHUB_URL}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Github
-      </a>
+      {socials.map(({ id, url }, indx) => (
+        <a href={url} key={indx} target="_blank" className="mr-2 mt-2">
+          <Image src={`/socials/${id}.svg`} alt={id} width={32} height={32} />
+        </a>
+      ))}
     </footer>
   );
 };
