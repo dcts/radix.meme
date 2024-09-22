725980e9451798103ae36d313ca1957940f06580
siegfriedbz <siegfried.bozza@yahoo.com>
2024-09-22 14:39:44 +0200

    add effects

--- components/CreateCoinForm.tsx
+++ components/CreateCoinForm.tsx
@@ -1,14 +1,15 @@
 "use client";
 
+import * as React from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { CreateCoinInputSchema } from "@/app/_zod";
 import { createCoinAction } from "@/app/_actions/createCoinAction";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import { Label } from "@radix-ui/react-label";
-import { Textarea } from "@/components/ui/textarea";
 import { HiMiniRocketLaunch } from "react-icons/hi2";
+import { cn } from "@/lib/utils";
+import { useMotionTemplate, useMotionValue, motion } from "framer-motion";
 
 // TODO display validation errors
 // TODO set submit btn disabled state
@@ -85,7 +86,9 @@ const CreateCoinForm = () => {
         />
       </div>
 
-      <span>Attention: Coin data cannot be changed after creation</span>
+      <span className="text-white/80">
+        Attention: Coin data cannot be changed after creation
+      </span>
       <Button
         type="submit"
         className="btn bg-dexter-gradient-green/80 hover:bg-dexter-gradient-green
@@ -99,3 +102,113 @@ const CreateCoinForm = () => {
 };
 
 export default CreateCoinForm;
+
+// Input component extends from shadcnui - https://ui.shadcn.com/docs/components/input
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
+    const radius = 100; // change this to increase the rdaius of the hover effect
+    const [visible, setVisible] = React.useState(false);
+
+    const mouseX = useMotionValue(0);
+    const mouseY = useMotionValue(0);
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    function handleMouseMove({ currentTarget, clientX, clientY }: any) {
+      const { left, top } = currentTarget.getBoundingClientRect();
+
+      mouseX.set(clientX - left);
+      mouseY.set(clientY - top);
+    }
+    return (
+      <motion.div
+        style={{
+          background: useMotionTemplate`
+        radial-gradient(
+          ${visible ? radius + "px" : "0px"} circle at ${mouseX}px ${mouseY}px,
+          var(--blue-500),
+          transparent 80%
+        )
+      `,
+        }}
+        onMouseMove={handleMouseMove}
+        onMouseEnter={() => setVisible(true)}
+        onMouseLeave={() => setVisible(false)}
+        className="p-[2px] rounded-lg transition duration-300 group/input"
+      >
+        <input
+          type={type}
+          className={cn(
+            `flex h-10 w-full border-none bg-gray-50 dark:bg-zinc-800 text-black dark:text-white shadow-input rounded-md px-3 py-2 text-sm  file:border-0 file:bg-transparent 
+          file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 
+          focus-visible:outline-none focus-visible:ring-[2px]  focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600
+           disabled:cursor-not-allowed disabled:opacity-50
+           dark:shadow-[0px_0px_1px_1px_var(--neutral-700)]
+           group-hover/input:shadow-none transition duration-400
+           `,
+            className
+          )}
+          ref={ref}
+          {...props}
+        />
+      </motion.div>
+    );
+  }
+);
+Input.displayName = "Input";
+
+export interface TextAreaProps
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextAreaProps>(
+  ({ className, ...props }, ref) => {
+    const radius = 100; // change this to increase the rdaius of the hover effect
+    const [visible, setVisible] = React.useState(false);
+
+    const mouseX = useMotionValue(0);
+    const mouseY = useMotionValue(0);
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    function handleMouseMove({ currentTarget, clientX, clientY }: any) {
+      const { left, top } = currentTarget.getBoundingClientRect();
+
+      mouseX.set(clientX - left);
+      mouseY.set(clientY - top);
+    }
+    return (
+      <motion.div
+        style={{
+          background: useMotionTemplate`
+        radial-gradient(
+          ${visible ? radius + "px" : "0px"} circle at ${mouseX}px ${mouseY}px,
+          var(--blue-500),
+          transparent 80%
+        )
+      `,
+        }}
+        onMouseMove={handleMouseMove}
+        onMouseEnter={() => setVisible(true)}
+        onMouseLeave={() => setVisible(false)}
+        className="p-[2px] rounded-lg transition duration-300 group/input"
+      >
+        <textarea
+          className={cn(
+            `flex h-32 w-full border-none bg-gray-50 dark:bg-zinc-800 text-black dark:text-white shadow-input rounded-md px-3 py-2 text-sm  file:border-0 file:bg-transparent 
+              file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 
+              focus-visible:outline-none focus-visible:ring-[2px]  focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600
+              disabled:cursor-not-allowed disabled:opacity-50
+              dark:shadow-[0px_0px_1px_1px_var(--neutral-700)]
+              group-hover/input:shadow-none transition duration-400
+           `,
+            className
+          )}
+          ref={ref}
+          {...props}
+        />
+      </motion.div>
+    );
+  }
+);
+Textarea.displayName = "Textarea";
