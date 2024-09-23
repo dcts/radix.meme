7004477d37c11c804db423932ac7b81e2119f692
siegfriedbz <siegfried.bozza@yahoo.com>
2024-09-23 22:03:18 +0200

    add favicon + basic metadata

--- app/token/[address]/page.tsx
+++ app/token/[address]/page.tsx
@@ -1,4 +1,6 @@
-// import type { TTokenData } from "../../../types";
+import type { TTokenData } from "../../../types";
+
+import { devCoinsData } from "@/data";
 
 type TProps = {
   params: {
@@ -8,9 +10,21 @@ type TProps = {
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const getData = async (address: string) => {
-  const token = undefined;
+  const token = devCoinsData.at(0) as TTokenData;
   return token;
 };
+
+export async function generateMetadata({ params }: TProps) {
+  const { address } = params;
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const token = await getData(address);
+
+  return {
+    title: `DeXter Launchpad | Token | ${token.name}`,
+    description: token.description,
+  };
+}
 
 const page = async ({ params }: TProps) => {
   const { address } = params;
@@ -18,7 +32,13 @@ const page = async ({ params }: TProps) => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const token = await getData(address);
 
-  return <div>Token details/trade page</div>;
+  return (
+    <div>
+      Token details/trade page
+      <div>{token.name}</div>
+      <div>{token.description}</div>
+    </div>
+  );
 };
 
 export default page;
