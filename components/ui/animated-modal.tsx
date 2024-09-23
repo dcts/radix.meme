de83a2b0e95bdd2feb80a593621ffea2796dac41
saidam90 <saida.murtazaliyeva@yahoo.com>
2024-09-23 15:13:43 +0200

    Fix eslint error

--- components/ui/animated-modal.tsx
+++ components/ui/animated-modal.tsx
@@ -221,12 +221,12 @@ const CloseIcon = () => {
 // Add it in a separate file, I've added here for simplicity
 export const useOutsideClick = (
   ref: React.RefObject<HTMLDivElement>,
-  callback: Function
+  callback: (event: MouseEvent | TouchEvent) => void
 ) => {
   useEffect(() => {
-    const listener = (event: any) => {
+    const listener = (event: MouseEvent | TouchEvent) => {
       // DO NOTHING if the element being clicked is the target element or their children
-      if (!ref.current || ref.current.contains(event.target)) {
+      if (!ref.current || ref.current.contains(event.target as Node)) {
         return;
       }
       callback(event);
