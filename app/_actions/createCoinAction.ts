6524d8baa430d67b45e7f8963bb964e3acdf8daf
siegfriedbz <siegfried.bozza@yahoo.com>
2024-09-24 20:46:54 +0200

    transient fix create-coin-form + server-action

--- app/_actions/createCoinAction.ts
+++ app/_actions/createCoinAction.ts
@@ -1,11 +1,14 @@
 "use server";
 
+import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
 import { pinata } from "../../lib/pinata/config";
 
-// TODO handle server-side zod validation -> https://next-safe-action.dev/docs/integrations/react-hook-form
-// TODO handle pinata errors
+// TODO SIEG handle server-side zod validation -> https://next-safe-action.dev/docs/integrations/react-hook-form
+// TODO SIEG handle pinata errors
 
 export async function createCoinAction(formData: FormData) {
+  /** extract data from client sent form-data */
   // const name = formData.get("name");
   // const symbol = formData.get("symbol");
   // const description = formData.get("description");
@@ -14,15 +17,30 @@ export async function createCoinAction(formData: FormData) {
   // const website = formData.get("website");
   const imageFile = formData.get("image") as File;
 
-  // upload img to pinata => imgUrl
-  const uploadData = await pinata.upload.file(imageFile);
-  const imageUrl = await pinata.gateways.convert(uploadData.IpfsHash);
-  console.log("imageUrl", imageUrl);
+  /** initialize token data that should be returned from radix TRX */
+  let newToken = undefined;
 
-  // create on-chain TRX
+  try {
+    /** 1. upload img to pinata => imgUrl */
+    const uploadData = await pinata.upload.file(imageFile);
+    const imageUrl = await pinata.gateways.convert(uploadData.IpfsHash);
+    console.log("imageUrl", imageUrl);
 
-  // return response to client
-  return {
-    status: "success",
-  };
+    /** 2. create on-chain TRX => get token address */
+    // TODO THOMAS
+    newToken = { address: "0x..." };
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log(error.message);
+    }
+    // TODO SIEG || SAIDA
+    /** Handle errors - return response to client - MUST BE STRINGiFYABLE */
+    return {
+      status: "error",
+    };
+  }
+
+  // 4. redirect to /token/:address page --- https://nextjs.org/docs/app/building-your-application/routing/redirecting#redirect-function
+  revalidatePath("/"); // Update cached tokens in home page
+  redirect(`/token/${newToken.address}`); // Navigate to the new token page
 }
