c3675c94a076a3b1d8e03c895b116cc0537225f2
Thomas <thomas@dcts.dev>
2024-09-24 18:56:15 +0200

    init subscriptions with env variable + add NEXT_PUBLIC to make them available in the browser

--- app/_store/subscriptions.ts
+++ app/_store/subscriptions.ts
@@ -31,25 +31,35 @@ export function getGatewayApiClientOrThrow() {
 let subs: Subscription[] = [];
 
 export function initializeSubscriptions(store: AppStore) {
+  // Get network from environment
+  const networkId = {
+    mainnet: RadixNetwork.Mainnet,
+    stokenet: RadixNetwork.Stokenet,
+  }[process.env.NEXT_PUBLIC_NETWORK!] || RadixNetwork.Stokenet;
+
   // Initialize rdt instance (specify to which app it connects)
   rdt = RadixDappToolkit({
     dAppDefinitionAddress:
       "account_tdx_2_129kev9w27tsl7qjg0dlyze70kxnlzycs8v2c85kzec40gg8mt73f7y",
-    networkId: RadixNetwork.Stokenet,
+    networkId: networkId,
     featureFlags: ["ExperimentalMobileSupport"],
   });
+  
   // Single VS multi account support
   // -> accounts().exactly(1) -> single account
   // -> accounts().atLeast(1) -> multi account
   rdt.walletApi.setRequestData(
     DataRequestBuilder.accounts().exactly(1)
   );
+  
   // Initialize gateway api client
   gatewayApiClient = GatewayApiClient.initialize(
     rdt.gatewayApi.clientConfig
   );
+  
   // Set connect button theme
   rdt.buttonApi.setTheme("white");
+  
   // Subscribe to connect button updates (if user loggs out, updates data sharing etc...)
   subs.push(
     rdt.walletApi.walletData$.subscribe((walletData: WalletData) => {
