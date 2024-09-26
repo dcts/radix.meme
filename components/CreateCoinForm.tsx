01abc8df98f7f3fb4847729b00fccd07f5430af6
Thomas <thomas@dcts.dev>
2024-09-26 08:08:37 +0200

    fix linter

--- components/CreateCoinForm.tsx
+++ components/CreateCoinForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, forwardRef, InputHTMLAttributes, useEffect } from "react";
+import { useState, forwardRef, InputHTMLAttributes } from "react";
 import { useRouter } from "next/navigation";
 import { FieldValues, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
