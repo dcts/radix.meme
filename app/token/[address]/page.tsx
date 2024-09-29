17abde1b05b4d9b8b17fdc4fa9a3562236b94a2c
Thomas <thomas@dcts.dev>
2024-09-29 03:31:13 +0200

    server side fetching for tokenDetails page

--- app/token/[address]/page.tsx
+++ app/token/[address]/page.tsx
@@ -1,10 +1,12 @@
 import TokenDetails from "@/components/TokenDetails";
+import { getTokenData } from "@/utils/api-calls";
 import { Metadata } from "next";
 
 type TProps = {
   params: {
     address: string;
   };
+  searchParams?: { [key: string]: string };
 };
 
 export const metadata: Metadata = {
@@ -12,15 +14,24 @@ export const metadata: Metadata = {
   description: "The first meme fair launch platform on Radix",
 };
 
-const page = async ({ params }: TProps) => {
-  const { address } = params;
+// Server component
+const tokenDetails = async ({ searchParams }: TProps) => {
+  const componentAddressSearchParam = searchParams?.componentAddress;
+
+  const componentAddress = Array.isArray(componentAddressSearchParam)
+    ? componentAddressSearchParam[0]
+    : componentAddressSearchParam;
+  if (!componentAddress) {
+    throw new Error("Invalid component address");
+  }
+  const tokenData = await getTokenData(componentAddress);
 
   return (
     <div className="px-8 sm:px-20">
-      {/* <h1>Token details</h1> */}
-      <TokenDetails tokenAddress={address} />
+      {/* Client component */}
+      <TokenDetails tokenData={tokenData} />
     </div>
   );
 };
 
-export default page;
+export default tokenDetails;
