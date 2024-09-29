9d386716de97faf88ed26cb91eb2addee0f1133f
Thomas <thomas@dcts.dev>
2024-09-29 11:52:55 +0200

    add no caching via nextJS config

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,19 @@ const nextConfig = {
   // For some reason this does not play well with dApp toolkit.
   // https://discord.com/channels/417762285172555786/1181329944811413555/1181336985609175080
   reactStrictMode: false,
-  
+
+  headers: () => [
+    {
+      source: '/',
+      headers: [
+        {
+          key: 'Cache-Control',
+          value: 'no-store',
+        },
+      ],
+    },
+  ],
+
   images: {
     remotePatterns: [
       {
