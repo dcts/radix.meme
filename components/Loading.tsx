59b7a6b1eb771e5445abdc6865c8e0a131f7c434
saidam90 <saida.murtazaliyeva@yahoo.com>
2024-10-13 20:32:18 +0400

    Reduce the size of the loading symbol

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,7 +1,7 @@
 import { PulseLoader } from "react-spinners";
 
 const Loading = () => {
-  return <PulseLoader size="1.25rem" color="#474C52" />; // "dexter-gray"
+  return <PulseLoader size="0.5rem" color="#474C52" />; // "dexter-gray"
 };
 
 export default Loading;
